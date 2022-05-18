100def9527af8efe8ef7f66d4822861bb75e08f0
Raul Chiarella <RaulPotter11@Gmail.com>
2022-05-18 17:39:22 -0300

    Limpeza Automática

--- moveLogs.sh
+++ moveLogs.sh
@@ -1,8 +1,8 @@
-# Crontab a cada 10 minutos
-# */10 * * * 0 su - -c "sh /usr/local/bin/moveLog.sh >/dev/null 2> /dev/null"
+# Crontab todo domingo a meia noite
+# 0 0 * * SUN su - -c "sh /usr/local/bin/moveLog.sh >/dev/null 2> /dev/null"
 
 # Compacta em gz os logs
-tar -czvf /var/log/backupLogs-`date +%Y-%m-%d`.gz /var/log/cron-202* /var/log/httpd/*_log*
+tar -czvf /var/log/backupLogs-`date +%Y-%m-%d`.gz /var/log/cron-202* /var/log/messages-202* /var/log/httpd/*_log*
 
 # Cria pasta de log e move o arquivo
 mkdir /mnt/dados/logs/backupLogs-`date +%Y-%m-%d`
